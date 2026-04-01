d03a9353f1c8adb813b09939f04a175d09603dcc
Fathima Shehna <shehnafathima3@gmail.com>
2026-04-01 10:59:11 +0530

    Query for users with challenges

---
+++ HomeWork6-mobileApp.sql
@@ -0,0 +1,5 @@
+INSERT INTO `mobile_app`(`id`, `name`, `city`, `score`, `bonus`, `challenge`) VALUES (1,'Raj','Chennai',88,5,'Fitness'),(2,'Anu','Mumbai',91,NULL,'Diet'),(3,'Ravi','Chennai',78,3,'Fitness'),(4,'Meena','Delhi',82,NULL,'Diet'),(5,'Farah','Mumbai',95,4,'Fitness'),(6,'Kiran','Pune',70,NULL,'Yoga'),(7,'Latha','Pune',87,NULL,'Fitness');
+
+SELECT * FROM `mobile_app` HAVING `score` > AVG(score);
+
+SELECT name,challenge FROM mobile_app WHERE challenge = (SELECT challenge FROM mobile_app WHERE `name` = 'Farah');
