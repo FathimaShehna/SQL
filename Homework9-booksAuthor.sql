8a7f0c42af9b235fd927dfaaa9ff39dcf36ab34a
Fathima Shehna <shehnafathima3@gmail.com>
2026-04-06 11:20:30 +0530

    test

--- Homework9-booksAuthor.sql
+++ Homework9-booksAuthor.sql
@@ -2,4 +2,6 @@ CREATE TABLE `authors` (`author_id` int not null PRIMARY KEY, `author_name` varc
 
 CREATE TABLE books_author (book_id int not null PRIMARY KEY, book_title varchar(50), author_id int not null, FOREIGN KEY (author_id) REFERENCES authors (author_id), UNIQUE (book_id));
 
+INSERT INTO `authors`(`author_id`, `author_name`, `email`) VALUES (101,'abc','tom@test'),(102,'test','tom@test');
+
 
