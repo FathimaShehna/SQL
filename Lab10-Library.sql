d633419dd18775d9a53453acd6c8961be07dcebc
Fathima Shehna <shehnafathima3@gmail.com>
2026-04-06 13:00:19 +0530

    test

--- Lab10-Library.sql
+++ Lab10-Library.sql
@@ -3,3 +3,5 @@ CREATE TABLE library (`author_id` int not null PRIMARY KEY AUTO_INCREMENT,`name`
 ALTER TABLE library AUTO_INCREMENT=101;
 
 CREATE TABLE books_library (book_id int not null PRIMARY KEY AUTO_INCREMENT, title varchar (50), author_id int, FOREIGN KEY (author_id) REFERENCES library (author_id));
+
+ALTER TABLE books_library ADD INDEX indx (author_id);
